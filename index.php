1b149b2fd8888453c3307fd9a5212f8c4eb70973
doumx <admoon28@gmail.com>
2025-08-10 20:50:01 +0000

    Add initial PHP script to output greeting messages

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+echo 'Hello World!';
+echo ' This is a simple PHP script.';
+echo ' It outputs a greeting message.';
+echo ' Enjoy coding with PHP!';
+echo ' Have a great day!';
+echo ' Keep learning and exploring new things.';
+echo ' PHP is a versatile language.';
